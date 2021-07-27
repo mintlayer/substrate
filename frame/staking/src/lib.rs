e80fca0c2105cab2a78481fdd842ff3215d5ffed
André Silva <andrerfosilva@gmail.com>
2021-07-27 18:39:33 +0100

    staking: add logic to track offending validators

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -617,14 +617,13 @@ pub struct UnappliedSlash<AccountId, Balance: HasCompact> {
 ///
 /// This is needed because `Staking` sets the `ValidatorIdOf` of the `pallet_session::Config`
 pub trait SessionInterface<AccountId>: frame_system::Config {
-	/// Disable a given validator by stash ID.
-	///
-	/// Returns `true` if new era should be forced at the end of this session.
-	/// This allows preventing a situation where there is too many validators
-	/// disabled and block production stalls.
-	fn disable_validator(validator: &AccountId) -> Result<bool, ()>;
+	/// Disable the validator at the given index, returns `false` if the validator was already
+	/// disabled.
+	fn disable_validator(validator_index: usize) -> bool;
 	/// Get the validators from session.
 	fn validators() -> Vec<AccountId>;
+	/// Get the number of validators in the session.
+	fn validators_len() -> usize;
 	/// Prune historical session tries up to but not including the given index.
 	fn prune_historical_up_to(up_to: SessionIndex);
 }
@@ -643,12 +642,16 @@ where
 		Option<<T as frame_system::Config>::AccountId>,
 	>,
 {
-	fn disable_validator(validator: &<T as frame_system::Config>::AccountId) -> Result<bool, ()> {
-		<pallet_session::Pallet<T>>::disable(validator)
+	fn disable_validator(validator_index: usize) -> bool {
+		<pallet_session::Pallet<T>>::disable_index(validator_index)
 	}
 
 	fn validators() -> Vec<<T as frame_system::Config>::AccountId> {
 		<pallet_session::Pallet<T>>::validators()
+	}
+
+	fn validators_len() -> usize {
+		<pallet_session::Pallet<T>>::validators_len()
 	}
 
 	fn prune_historical_up_to(up_to: SessionIndex) {
@@ -916,6 +919,10 @@ pub mod pallet {
 		/// their reward. This used to limit the i/o cost for the nominator payout.
 		#[pallet::constant]
 		type MaxNominatorRewardedPerValidator: Get<u32>;
+
+		/// The fraction of the validator set that is safe to be offending.
+		/// After the threshold is reached a new era will be forced.
+		type OffendingValidatorsThreshold: Get<Perbill>;
 
 		/// Weight information for extrinsics in this pallet.
 		type WeightInfo: WeightInfo;
@@ -1207,6 +1214,9 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn current_planned_session)]
 	pub type CurrentPlannedSession<T> = StorageValue<_, SessionIndex, ValueQuery>;
+
+	#[pallet::storage]
+	pub(crate) type OffendingValidators<T: Config> = StorageValue<_, Vec<u32>, ValueQuery>;
 
 	/// True if network has been upgraded to this version.
 	/// Storage version of the pallet.
@@ -2568,6 +2578,8 @@ impl<T: Config> Pallet<T> {
 					Self::end_era(active_era, session_index);
 				}
 			}
+
+			<OffendingValidators<T>>::take();
 		}
 	}
 
