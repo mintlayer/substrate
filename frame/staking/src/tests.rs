2cae9b9216ccba43a92dfd6fbea467f15e4ab5cc
André Silva <andrerfosilva@gmail.com>
2021-07-27 23:57:55 +0100

    staking: fix adding offending validator when already slashed in era

--- frame/staking/src/tests.rs
+++ frame/staking/src/tests.rs
@@ -2276,10 +2276,11 @@ fn slash_in_old_span_does_not_deselect() {
 			1,
 		);
 
-		// not forcing for zero-slash and previous span.
-		assert_eq!(Staking::force_era(), Forcing::NotForcing);
-		assert!(<Validators<Test>>::contains_key(11));
-		assert!(Session::validators().contains(&11));
+		// the validator doesn't get chilled again
+		assert!(<Staking as Store>::Validators::iter().find(|(stash, _)| *stash == 11).is_some());
+
+		// but we are still forcing a new era
+		assert_eq!(Staking::force_era(), Forcing::ForceNew);
 
 		on_offence_in_era(
 			&[OffenceDetails {
@@ -2294,7 +2295,9 @@ fn slash_in_old_span_does_not_deselect() {
 		// the validator doesn't get chilled again
 		assert!(<Staking as Store>::Validators::iter().find(|(stash, _)| *stash == 11).is_some());
 
-		// but we are still forcing a new era
+		// but it's disabled
+		assert!(is_disabled(10));
+		// and we are still forcing a new era
 		assert_eq!(Staking::force_era(), Forcing::ForceNew);
 	});
 }
